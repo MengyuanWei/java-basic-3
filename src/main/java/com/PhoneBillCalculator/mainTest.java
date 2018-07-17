5456572c4854bdcdc5d0cbd6db0f4296cfb5bc12
张正十 <zhangzhengshi@juus-MacBook-puro.local>
2018-07-17 14:04:16 -0400

    Phone bill Calculator from codility

---
+++ src/main/java/com/PhoneBillCalculator/mainTest.java
@@ -0,0 +1,8 @@
+package com.PhoneBillCalculator;
+
+public class mainTest {
+    public static void main(String args[]) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution("00:01:07,400-234-090\n00:05:01,701-080-080\n00:05:00,400-234-090"));
+    }
+}
