e8649b8848f5df8c7fd1954fbed9bc9ba1489c51
张正十 <zhangzhengshi@juusMacBookpuro.router>
2018-07-23 13:05:30 -0400

    StackPractice

---
+++ src/main/java/com/corejava/TimeFlys.java
@@ -0,0 +1,28 @@
+package com.corejava;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class TimeFlys {
+    public static void main (String[] args){
+        String startDate = "03-15-19";
+        String endData = "05-12-19";
+        DateTimeFormatter startFormatter = DateTimeFormatter.ofPattern("MM-dd-yy");
+        DateTimeFormatter endFormatter = DateTimeFormatter.ofPattern("MM-dd-yy");
+        LocalDate startDateTime= LocalDate.parse(startDate,startFormatter);
+        LocalDate endDateTime = LocalDate.parse(endData,endFormatter);
+        LocalDate compareDate = startDateTime;
+        while(compareDate.isBefore(endDateTime)){
+        startDateTime.plusDays(7);
+        compareDate=compareDate.plusDays(7);
+        if(compareDate.isAfter(endDateTime)){
+            compareDate=endDateTime;
+            System.out.println(startDateTime.toString()+"--"+compareDate.toString()+"\n");
+            break;
+        }else {
+            System.out.println(startDateTime.toString()+"--"+compareDate.toString()+"\n");
+        }
+        startDateTime = compareDate;
+        }
+    }
+}
---
+++ src/main/java/com/corejava/TimeFlysPlus.java
@@ -0,0 +1,8 @@
+package com.corejava;
+public class TimeFlysPlus {
+    public static void main (String[] args){
+
+    }
+
+
+}
