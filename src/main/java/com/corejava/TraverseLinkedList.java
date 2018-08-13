2815d0cd538dfcb2a7d3536e81f8c3707952eddd
morgan <zhengsz@vt.edu>
2018-08-12 22:35:30 -0400

    LinkedList

---
+++ src/main/java/com/corejava/TraverseLinkedList.java
@@ -0,0 +1,30 @@
+package com.corejava;
+
+import java.util.LinkedList;
+
+public class TraverseLinkedList {
+    Node head;
+
+    static class Node{
+        int data;
+        Node next;
+        Node(int b){
+            data = b;
+            next=null;
+        }
+
+        public void printList(){
+            Node node = ;
+        }
+
+        public static void main(String[] args){
+            LinkedList<Integer> linkedList= new LinkedList<>();
+            TraverseLinkedList traverseLinkedList=new TraverseLinkedList();
+            traverseLinkedList.head=new Node(1);
+            Node second=new Node(2);
+            Node third = new Node(3);
+            traverseLinkedList.head.next=second;
+            second.next=third;
+        }
+    }
+}
